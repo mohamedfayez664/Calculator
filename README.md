42a7c025001724a89e9b69414e18641f6c7cea51
mohamed fayez <mohamedfayez664@gmail.com>
2019-11-13 15:39:14 +0200

    Calculater 1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+Calculator
+
+this calcultor make simple operations like / * - + in deciemal numbers 
+if you want any question just send me a message on moamedfayez664@gmail.com
---
+++ calc_code/calc/Debug/main.d
@@ -0,0 +1,14 @@
+main.o main.o: ../main.c ../keypad.h ../std_types.h ../std_macros.h \
+  ../std_micro_comp_conf.h ../lcd.h ../delay.h
+
+../keypad.h:
+
+../std_types.h:
+
+../std_macros.h:
+
+../std_micro_comp_conf.h:
+
+../lcd.h:
+
+../delay.h:
